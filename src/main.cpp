afe5500b87fca97a5282f5982f50dd3e16b870cc
capta1nseal <capta1nseal@psil.fi>
2023-05-24 13:12:40 +0300

    Comment imports and add gitignore to project root

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,15 @@
 // debugging and command line interface
 #include <iostream>
 
-
+// logic processing for cell restrictions
 #include "cell.cpp"
 
+// data and logic for the grid
 #include "grid.cpp"
 
+// user interface for the algorithms
 #include "application.cpp"
+
 
 int main(int argc, char* argv[])
 {
